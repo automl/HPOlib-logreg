d2d7748bcd8637b5afc52f7af622027415e6cee3
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2014-10-08 12:47:41 +0200

    Update config files

--- hyperopt_august2013_mod/logistic_hyperopt.py
+++ hyperopt_august2013_mod/logistic_hyperopt.py
@@ -1,6 +1,8 @@
 from hyperopt import hp
+from hyperopt.pyll import scope
+
 
 space = {"lrate" : hp.uniform("lrate", 0, 10),
          "l2_reg" : hp.uniform("l2_reg", 0, 1),
-         "batchsize" : hp.uniform("batchsize", 20, 2000),
-         "n_epochs" : hp.uniform("n_epochs", 5, 2000)}
+         "batchsize" : scope.int(hp.quniform("batchsize", 20, 2000, 1)),
+         "n_epochs" : scope.int(hp.quniform("n_epochs", 5, 2000, 1))}
